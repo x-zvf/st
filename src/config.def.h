209e8faf1d574159fe885b02e1d82f0c672e38f6
octalgon <octalgon@protonmail.com>
2018-12-21 21:49:23 +0100

    TEMPORARILY RETURNED TO VANILLA, BECAUSE I FUCKED UP PATCHING A COUPLE OF COMMITS BACK. REDOING EVERYTHING

--- src/config.def.h
+++ src/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
-static int borderpx = 0;
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -82,34 +82,44 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-        /* solarized dark */
-        "#073642",  /*  0: black    */
-        "#dc322f",  /*  1: red      */
-        "#859900",  /*  2: green    */
-        "#b58900",  /*  3: yellow   */
-        "#268bd2",  /*  4: blue     */
-        "#d33682",  /*  5: magenta  */
-        "#2aa198",  /*  6: cyan     */
-        "#eee8d5",  /*  7: white    */
-        "#002b36",  /*  8: brblack  */
-        "#cb4b16",  /*  9: brred    */
-        "#586e75",  /* 10: brgreen  */
-        "#657b83",  /* 11: bryellow */
-        "#839496",  /* 12: brblue   */
-        "#6c71c4",  /* 13: brmagenta*/
-        "#93a1a1",  /* 14: brcyan   */
-        "#fdf6e3",  /* 15: brwhite  */
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
 };
 
-unsigned int defaultfg = 12;
-unsigned int defaultbg = 8;
-static unsigned int defaultcs = 14;
-static unsigned int defaultrcs = 15;
 
-
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
@@ -146,14 +156,8 @@ static unsigned int defaultattr = 11;
  */
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            string */
-	/*{ Button4,              XK_ANY_MOD,     "\031" },
-	{ Button5,              XK_ANY_MOD,     "\005" }, */
-};
-
-MouseKey mkeys[] = {
-        /* button               mask            function        argument */
-        { Button4,              ShiftMask,      kscrollup,      {.i =  1} },
-        { Button5,              ShiftMask,      kscrolldown,    {.i =  1} },
+	{ Button4,              XK_ANY_MOD,     "\031" },
+	{ Button5,              XK_ANY_MOD,     "\005" },
 };
 
 /* Internal keyboard shortcuts. */
