ad4f5a78bf579143b6f2d1064385ddf027ee3df5
octalgon <octalgon@protonmail.com>
2019-01-26 23:30:39 +0100

    Lowered opacity

--- src/config.h
+++ src/config.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0x55;
+unsigned int alpha = 0xAA;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
