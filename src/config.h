4a178f4b9bae31bf7363994982001685c453cb5b
xzvf <xzvf@su-root.cc>
2021-01-07 17:02:12 +0100

    No alpha

--- src/config.h
+++ src/config.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xAA;
+unsigned int alpha = 0xFF;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
